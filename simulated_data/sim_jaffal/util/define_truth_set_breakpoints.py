c1d2727710e265a9a39ec1e283d905e58b51c7a9
Brian Haas <brianjohnhaas@gmail.com>
2023-08-25 09:57:58 -0400

    adding define_truth_set_breakpoints.py

--- simulated_data/sim_jaffal/util/define_truth_set_breakpoints.py
+++ simulated_data/sim_jaffal/util/define_truth_set_breakpoints.py
@@ -14,11 +14,26 @@ from Bio import SeqIO  # type: ignore
 from intervaltree import Interval  # type: ignore
 from liftover import get_lifter  # type: ignore
 import re
+import glob
+import sys
+import gzip
 
 
+def main():
+    template_sequences = "/seq/RNASEQ/public_ftp/STAR_FUSION_PAPER/SupplementaryData/sim_reads/fusion_transcript_sequences/*.fasta.gz"
 
+    gold_standard_brkpts = load_gold_standard(template_sequences)
 
-converter = get_lifter("hg19", "hg38")
+    gold_standard_brkpts.to_csv("/seq/RNASEQ/public_ftp/STAR_FUSION_PAPER/SupplementaryData/sim_reads/fusion_transcript_sequences/fusion_breakpoint_summary.tsv", sep="\t", index=False)
+
+    sys.exit(0)
+    
+
+"""
+Example fasta header:
+>LCORL|ENSG00000178177.10--CT47B1|ENSG00000236446.2 LCORL|ENST00000382224.1--CT47B1|ENST00000371311.3 chr4:18022357-18022154,17974508-17974443,17964672-17964593,17963655-17963526[-] chrX:120007874-120007719,120006594-120006457[-] FusedAt:480
+"""
+
 
 def load_gold_standard(template_sequences):
     """
@@ -31,78 +46,101 @@ def load_gold_standard(template_sequences):
     """
     gold_standard_breakpoints_hg19 = []
     for fasta in glob.glob(template_sequences):
-        os.path.basename(fasta).replace(".fasta.gz", "")
+        #print(fasta)
+        dataset = os.path.basename(fasta).replace(".fasta.gz", "")
         with gzip.open(fasta, "rt") as handle:
             for record in SeqIO.parse(handle, "fasta"):
                 desc = record.description.split()
                 # example: LCORL|ENSG00000178177.10--CT47B1|ENSG00000236446.2
-                one_gene_pair = re.search(r"(\S+)\|.*--(\S+)\|.*", desc[0])
-                if one_gene_pair:
-                    one_gene_pair = list(one_gene_pair.groups())
+                fusion_gene_pair = re.search(r"(\S+)\|.*--(\S+)\|.*", desc[0])
+                if fusion_gene_pair:
+                    fusion_gene_pair = list(fusion_gene_pair.groups())
                 else:
                     raise Exception
-                # example: chr4:18022357-18022154,17963655-17963526[-]
-                one_end = desc[2].split(":")
-                # example: chrX:120007874-120007719,120006594-120006457[-]
-                another_end = desc[3].split(":")
 
-                one_end_coord = re.search(r"(?<=-)(\S+)\[", one_end[1].split(",")[-1])
-                if one_end_coord:
-                    one_end_coord = one_end_coord.groups()
+                left_gene, right_gene = fusion_gene_pair
+
+                ###########################
+                ## get left breakpoint info
+                # example: chr4:18022357-18022154,17963655-17963526[-]
+
+                left_fusion_info = desc[2]
+                left_breakpoint_chrom = left_fusion_info.split(":")[0]
+
+                left_breakpoint_coord = re.search(r"-(\d+)\[", left_fusion_info.split(",")[-1])
+                if left_breakpoint_coord:
+                    left_breakpoint_coord = int(left_breakpoint_coord.groups()[0])
                 else:
                     raise Exception
+
+                ############################
+                ## get right breakpoint info
+                # example: chrX:120007874-120007719,120006594-120006457[-]
+
+                right_breakpoint_info = desc[3]
+                right_breakpoint_chrom = right_breakpoint_info.split(":")[0]
+                right_breakpoint_coord = int(right_breakpoint_info.split(":")[1].split(",")[0].split("-")[0])
+
+                
+                
                 gold_standard_breakpoints_hg19.append(
                     [
-                        one_end[0],
-                        int(one_end_coord[0]),
-                        another_end[0],
-                        int(another_end[1].split(",")[0].split("-")[0]),
-                        "--".join(one_gene_pair),
+                        left_gene,
+                        left_breakpoint_chrom,
+                        left_breakpoint_coord,
+                        right_gene,
+                        right_breakpoint_chrom,
+                        right_breakpoint_coord,
+                        "--".join(fusion_gene_pair),
+                        dataset,
+                        record.description
                     ]
                 )
-    # did not consider strand
-    # do we consider strand ?
+
     gold_standard_breakpoints_hg19 = pd.DataFrame(gold_standard_breakpoints_hg19)
+
+    gold_standard_breakpoints_hg19.columns = ["LeftGene", "LeftChrom", "Hg19_LeftBreakpointCoord",
+                                              "RightGene", "RightChrom", "Hg19_RightBreakpointCoord",
+                                              "FusionName", "SourceDataset", "FastaDescription"]
+                                             
+
     print(gold_standard_breakpoints_hg19.head())
-    gold_standard_breakpoints_hg38 = pd.concat(
-        [
-            gold_standard_breakpoints_hg19.apply(
-                lambda x: ":".join(list(map(str, converter[x[0]][x[1]][0]))[:2]), axis=1
-            ),
-            gold_standard_breakpoints_hg19.apply(
-                lambda x: ":".join(list(map(str, converter[x[2]][x[3]][0]))[:2]), axis=1
-            ),
-            gold_standard_breakpoints_hg19.iloc[:, -1],
-        ],
-        axis=1,
-    )
-    gold_standard_breakpoints_hg38.loc[
-        :, "Breakpoint"
-    ] = gold_standard_breakpoints_hg38.apply(lambda x: "--".join(x[:2]), axis=1)
-    gold_standard_breakpoints_hg38.loc[
-        :, "BreakpointLexSort"
-    ] = gold_standard_breakpoints_hg38.apply(lambda x: "--".join(sorted(x[:2])), axis=1)
-    gold_standard_breakpoints_hg38.loc[
-        :, "FusionLexSort"
-    ] = gold_standard_breakpoints_hg38.apply(
-        lambda x: "--".join(sorted(x[4].upper().split("--"))), axis=1
-    )
-    print(gold_standard_breakpoints_hg38.loc[:, "FusionLexSort"].shape)
-    print(gold_standard_breakpoints_hg38.loc[:, "FusionLexSort"].unique().shape)
-    gold_standard_breakpoints_hg38 = gold_standard_breakpoints_hg38.set_axis(
-        gold_standard_breakpoints_hg38.loc[:, "FusionLexSort"].str.upper(), axis="index"
-    )
-    print(gold_standard_breakpoints_hg38.head())
-    gold_standard_breakpoints_hg38.columns = [
-        "LeftBreakPoint",
-        "RightBreakPoint",
-        "FusionName",
-        "Breakpoint",
-        "BreakpointLexSort",
-        "FusionLexSort",
-    ]
-    gold_standard_breakpoints_hg38.loc[
-        :, "FusionName"
-    ] = gold_standard_breakpoints_hg38.loc[:, "FusionName"].str.upper()
+
+    
+    ## Convert to hg38 using liftover
+
+    converter = get_lifter("hg19", "hg38")
+    """
+    >>> converter['chr1'][20304873]
+    [('chr1', 19978380, '+')]
+    """
+        
+    def run_hg19_to_hg38_liftover(chrom, coord):
+        
+        converted_coord_info = converter[chrom][coord][0]
+        
+        converted_chrom = converted_coord_info[0]
+        converted_coord = converted_coord_info[1]
+
+        converted_breakpoint = f"{converted_chrom}:{converted_coord}"
+        
+        return converted_breakpoint
+
+
+    gold_standard_breakpoints_hg38 = gold_standard_breakpoints_hg19.copy()
+
+    gold_standard_breakpoints_hg38['Hg38_LeftBreakpoint'] = gold_standard_breakpoints_hg38.apply(
+        lambda x: run_hg19_to_hg38_liftover(x['LeftChrom'], x['Hg19_LeftBreakpointCoord']), axis=1)
+
+    gold_standard_breakpoints_hg38['Hg38_RightBreakpoint'] = gold_standard_breakpoints_hg38.apply(
+        lambda x: run_hg19_to_hg38_liftover(x['RightChrom'], x['Hg19_RightBreakpointCoord']), axis=1)
+    
+    print(gold_standard_breakpoints_hg38.head()) 
+    
     return gold_standard_breakpoints_hg38
 
+
+
+
+if __name__=='__main__':
+    main()
