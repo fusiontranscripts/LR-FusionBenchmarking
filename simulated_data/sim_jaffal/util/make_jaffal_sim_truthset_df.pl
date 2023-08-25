63c3cb52b05bce80abcf5aa49f944665b162057e
Brian Haas <brianjohnhaas@gmail.com>
2023-08-25 12:15:06 -0400

    expanded truth set with breakpoints defined

--- simulated_data/sim_jaffal/util/make_jaffal_sim_truthset_df.pl
+++ simulated_data/sim_jaffal/util/make_jaffal_sim_truthset_df.pl
@@ -4,7 +4,24 @@ use strict;
 use warnings;
 
 
-print "sample\tfusion\tnum_reads\n";
+my %read_breakpoint_info;
+# must run in the LongReadFusionSimulation/ dir.
+open (my $fh, "../fusion_breakpoint_summary.tsv") or die $!;
+my $header = <$fh>;
+chomp $header;
+while (<$fh>) {
+    my $line = $_;
+    chomp $line;
+    my @vals = split(/\t/, $line);
+    my $descr_line = $vals[8];
+    my @pts = split(/\s+/, $descr_line);
+    my $acc = $pts[0];
+    $read_breakpoint_info{$acc} = $line;
+}
+close $fh;
+
+
+print "$header\tsample\tfusion_id\tnum_reads\n";
 
 foreach my $file (<*.truthset>) {
     
@@ -37,7 +54,8 @@ foreach my $file (<*.truthset>) {
     while(<$fh>) {
         chomp;
         my ($fusion, $num_reads) = split(/\t/);
-        print join("\t", $dataset, $fusion, $num_reads) . "\n";
+        my $breakpoint_info = $read_breakpoint_info{$fusion} or die "Error, no breakpoint info for $fusion";
+        print join("\t", $breakpoint_info, $dataset, $fusion, $num_reads) . "\n";
     }
 }
 
