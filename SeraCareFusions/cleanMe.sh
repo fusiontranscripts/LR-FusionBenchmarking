80d66bbee8fd8cfd15f89f779f16dea0e494da43
Brian Haas <brianjohnhaas@gmail.com>
2023-10-07 09:51:06 -0400

    adding cleanme

---
+++ SeraCareFusions/cleanMe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf ./_*
+rm -f ./preds*
+#rm -f ./all.auc.*
+#rm -f ./okPara_ignoreUnsure*
+#rm -f ./ignoreUnsure*
+#rm -f auc_files.list  fusion_result_file_listing.dat  pipe.log
+
+
