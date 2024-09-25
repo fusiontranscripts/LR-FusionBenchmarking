3c1a19869ec3130cf25df5ee029a778aa42a898d
Brian Haas <brianjohnhaas@gmail.com>
2024-09-25 10:10:04 -0400

    adding makefile and cleaner

--- simulated_data/our_pbsim3_sims/pbio_pbsim3_part5/cleanMe.sh
+++ simulated_data/our_pbsim3_sims/pbio_pbsim3_part5/cleanMe.sh
@@ -5,5 +5,5 @@ set -ex
 rm -rf ./_*
 rm -f ./preds.*
 
-rm -f ./fusion_result_file_listing.dat ./max_F1_summary.tsv ./max_F1_summary.barplot.pdf ./max_F1_summary.linepoint.pdf ./breakpoint_maxF1_data.tsv ./breakpoint_maxF1_summary.pdf
+rm -f ./fusion_result_file_listing.dat ./max_F1_summary.tsv ./max_F1_summary.barplot.pdf ./max_F1_summary.linepoint.pdf ./breakpoint_* ./mean_PR_AUC_* ./pipe.log ./all_combined_results.*
 
