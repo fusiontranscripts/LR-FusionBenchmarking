ba9631f35cd5ab290d499917035439050da3eef3
Brian Haas <brianjohnhaas@gmail.com>
2024-09-25 10:08:10 -0400

    adding cleanme

---
+++ simulated_data/our_pbsim3_sims/ONT_pbsim3_part5/cleanMe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+rm -rf ./_*
+rm -f ./preds.*
+
+rm -f ./fusion_result_file_listing.dat ./max_F1_summary.tsv ./max_F1_summary.barplot.pdf ./max_F1_summary.linepoint.pdf ./breakpoint_* ./pipe.log ./all_combined_results.PR_AUC.tsv ./mean_PR_AUC_summary.tsv
+
+
